945647c1063c55a6cdc0d6f58a712096bfe28f3c
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-05-23 20:50:03 -0300

    Cambios en estilo Login. Revisar responsibidad

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,45 +11,52 @@ export default function Login() {
           <Nav.Link href="#action1" className={styles.No}>
             Ahora no
           </Nav.Link>
-          <div className={styles.logo}>
-            <Image src="/LogoP.png" width="100px" className={styles.logoP} />
+          <div >
+            <div className={styles.logos}>
+              <Image src="/LogoP.png" width="100px" className={styles.logoP} />
+            </div>
+            <div className={styles.logoP1}>
+              <Image src="/LogoW.png" width="180px" />
+            </div>
           </div>
-          <div className={styles.logoP1}>
-            <Image src="/LogoW.png" width="180px" />
+          <div className={styles.loginBody}>
+            <h6 className={styles.titulo}>Elegí cómo querés ingresar</h6>
+            <div className={styles.buttons}>
+              <MyButton
+                className={styles.btnNav}
+                variant="outline-dark"
+                color="light"
+                text="Iniciar sesión con Facebook"
+                icon=<Image
+                  src="\icons8-facebook-rodeado-de-círculo-48.png"
+                  width={"20px"}
+                  style={{ marginRight: "5px" }}
+                />
+              />
+              <MyButton
+                className={styles.btnNav}
+                variant="outline-dark"
+                color="black"
+                text="Continuar con Google"
+                icon=<Image
+                  src="/icons8-logo-de-google-94.png"
+                  width={"20px"}
+                  style={{ marginBottom: "5px" }}
+                />
+              />
+              <MyButton
+                className={styles.btnNav}
+                variant="outline-dark"
+                color="black"
+                text="Otro método"
+              />
+            </div>
+
+            <p className={styles.parrafo}>
+              Al continuar, aceptas nuestros politica de privacidad y términos
+              de uso.
+            </p>
           </div>
-          <h6 className={styles.titulo}>Elegí cómo querés ingresar</h6>
-          <div className={styles.buttons}>
-            <MyButton
-              className={styles.btnNav}
-              variant="outline-dark"
-              color="light"
-              text="Iniciar sesión con Facebook"
-              icon=<Image
-                src="\icons8-facebook-rodeado-de-círculo-48.png"
-                width={"20px"}
-                style={{ marginRight: "5px" }}
-              />
-            />
-            <MyButton
-              className={styles.btnNav}
-              variant="outline-dark"
-              color="black"
-              text="Continuar con Google"
-              icon=<Image
-                src="/icons8-logo-de-google-94.png"
-                width={"20px"}
-                style={{ marginBottom: "5px" }}
-              />
-            />
-            <MyButton
-              className={styles.btnNav}
-              variant="outline-dark"
-              color="black"
-              text="Otro método"
-            />
-                  </div>
-                  
-                  <p className={styles.parrafo}>Al continuar, aceptas nuestros politica de privacidad y términos de uso.</p>
         </div>
       </div>
     </>
