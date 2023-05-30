4c804288a09ade459ece6a997a81cfa47805f6ca
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-05-30 13:31:05 -0300

    Aplico función map a Tops

--- src/Components/Tops.js
+++ src/Components/Tops.js
@@ -2,49 +2,58 @@ import styles from "@/styles/Tops.module.css";
 import { Image } from "react-bootstrap";
 
 export default function Tops() {
+  let cards = [
+    {
+      img: "Ubicación.svg",
+      titulo: "Top Ciudades",
+      parrafo:
+        "La Plata, Rosario, San Isidro, Córdoba, Buenos Aires, Vicente Lopez.",
+    },
+    {
+      img: "Barrios.svg",
+      titulo: "Top Barrios",
+      parrafo:
+        " Palermo, Caballito, Belgrano, Recoleta, Microcentro, Nueva Córdoba.",
+    },
+    {
+      img: "comidas.svg",
+      titulo: "Top Comidas",
+      parrafo: "Sushi, Picadas, Empanadas, Desayunos, Helados, Pizzas.",
+    },
+  ];
+
   return (
     <>
       <div className={styles.cont}>
         <div className={styles.nuevo}>
-        <h5>Delivery que satisface los sentidos</h5><br></br>
-        <p>
-          ¿Con Hambre y nada te copa? Acá tu Comida Sabrosa, Hoy Puede Tener
-          otro Gusto. Pedi Ya, Te Llena el Corazón.
-        </p>
-       
-        <div className={styles.tops}>
-          <div className={styles.destacado}>
-            <Image src="Ubicación.svg" className={styles.imagen} />
-            <div>
-              <h6>Top Ciudades</h6>
-              <p>
-                La Plata, Rosario, San Isidro, Córdoba, Buenos Aires, Vicente
-                Lopez.
-              </p>
-            </div>
-          </div>
+          <h5>Delivery que satisface los sentidos</h5>
+          <br></br>
+          <p>
+            ¿Con Hambre y nada te copa? Acá tu Comida Sabrosa, Hoy Puede Tener
+            otro Gusto. Pedi Ya, Te Llena el Corazón.
+          </p> <br></br>
 
-          <div className={styles.destacado}>
-            <Image src="Barrios.svg" className={styles.imagen} />
-            <div>
-              <h6>Top Barrios</h6>
-              <p>
-                Palermo, Caballito, Belgrano, Recoleta, Microcentro, Nueva
-                Córdoba.
-              </p>
-            </div>
-          </div>
-
-          <div className={styles.destacado}>
-            <Image src="comidas.svg" className={styles.imagen}/>
-            <div>
-              <h6>Top Comidas</h6>
-              <p>Sushi, Picadas, Empanadas, Desayunos, Helados, Pizzas.</p>
-            </div>
-          </div>
+          {cards.map((card) => {
+            return (
+              <>
+                <div className={styles.tops}>
+                  <div className={styles.destacado}>
+                    <Image
+                      src={card.img}
+                      className={styles.imagen}
+                      key={card.img}
+                    />
+                    <div>
+                      <h6 key={card.titulo}>{card.titulo}</h6>
+                      <p key={card.parrafo}>{card.parrafo}</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
-        </div>
-        </div>
+      </div>
     </>
   );
 }
