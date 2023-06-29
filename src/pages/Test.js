50bcbdbb5e0fa66894487ddd04098ccd620d342a
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-06-29 02:06:38 -0300

    Elimino comentario

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -4,52 +4,6 @@ import { useState } from "react";
 // import WatsonChat from "./WatsonChat";
 
 export default function Test() {
-  //PRUEBAS/ EJERCICIOS
-
-  // const handleSubmit = (valores) => {
-  //   console.log("AppJs: ", valores);
-  // };
-  //     <Container component="section" maxWidth="sm">
-  //   <h2>Formulario de Registro</h2>
-  //   <CustomLog handleSubmit={handleSubmit} />
-  // </Container>
-
-  //Traer servicio de firestore
-//Crear un puntero al dato que queremos traer
-//Traer el dato con una promesa
-
-
-//NOS CONECTAMOS CON UN DOCUMENTO DE UNA COLECCION DE FIREBASE
-// import { getFirestore, doc, getDoc } from "firebase/firestore";
-  // const [data, setData] = useState({});
-  // const { detalleId } = useParams();
-  // useEffect(() => {
-  //     const queryDb = getFirestore()//Traer Firestore
-  //     const queryDoc = doc(queryDb, "Negocios", "Bo0Hg652pgnw1rkHH4Sl")//Apuntar al documento
-  //     getDoc(queryDoc)//Traer el doc 
-  //         //getDoc(doc(getFirestore(),"Negocios", "hUBvoX3L0I5KFpQ9UjPo"))//Ponemos todo junto y es lo mismo.
-  //         .then( res => setData({ id: res.id, ...res.data() }))
-  // }, [])
-  //MOSTRAMOS DATA 
-  {/* <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Img variant="top" src={data.imagen}  />
-            <Card.Body>
-            <Card.Title>{data.nombre}</Card.Title>
-              <Card.Text>
-                {data.descripcion}
-            </Card.Text>
-            <Card.Text>
-                {data.precio}
-            </Card.Text>         
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row> */}
-
   const [nombre, setNombre] = useState("");
   const cambiarNombre = (nuevoNombre) => {
     setNombre(nuevoNombre);
