9337b22780e0d739efcd36d1fa0bc6e4ffa418e6
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-07-04 01:47:24 -0300

    Modifico UnProducto Component por error en render

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const Item = () => {
+const Item = ({data}) => {
   return (
-    <div>Item</div>
+<></>
   )
 }
 
--- src/pages/UnProducto.js
+++ src/pages/UnProducto.js
@@ -5,30 +5,30 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { CardImg } from "react-bootstrap";
 
 const UnProducto = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState({});
   const { detalleId } = useParams();
   useEffect(() => {
     const queryDb = getFirestore(); //Traer Firestore
     const queryDoc = doc(queryDb, "Negocios", "LEKNypbdxUj2f8MkHxEa"); //Apuntar al documento
     getDoc(queryDoc).then((res) => setData({ id: res.id, ...res.data() }));
   }, [detalleId]);
-
   return (
     <div className={styles.producto}>
-      <Card style={{ width: "17rem" }}>
-        <Card.Img variant="top" src={data.imagen} />
+      
+  <Card style={{ width: "17rem" }}>
+        <Card.Img variant="top" style={{height:"300px", marginBottom:"50px"}} src={data.imagen} />
         <Card.Body className={styles.cardBody}>
           <Card.Title>{data.nombre}</Card.Title>
           <Card.Text>{data.descripcion}</Card.Text>
-          <Card.Text>
+          <Card.Text style={{ marginTop:"50px", fontSize:"20px", textAlign:"left"}} >
             <strong>$ {data.precio}</strong>
           </Card.Text>
         </Card.Body>
       </Card>
-      <div> <Card.Img variant="top" src={data.infoNutricional} className={styles.info} />
-      </div>
+      <Card.Img variant="top" className={styles.info} src={data.infoNutricional} />
     </div>
   );
 };
