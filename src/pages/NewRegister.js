09cd572f7bc23d20d2252f0add59d416f4efd731
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-09-19 11:52:58 -0300

    Cambio ruta en registro

--- src/pages/NewRegister.js
+++ src/pages/NewRegister.js
@@ -8,17 +8,34 @@ import { useAuthContextProvider } from "../Context/AuthContext";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/router";
 
-
 const NewRegister = (props) => {
   const { user } = useAuthContextProvider();
   let { setUser } = useAuthContextProvider();
   let { signup } = useAuthContextProvider();
-  const{ auth } = useAuthContextProvider();
+  const { auth } = useAuthContextProvider();
   const [error, setError] = useState("");
   const router = useRouter("");
-  
+
   const handleSubmit = async (e) => {
-    await signup(user.email, user.password) 
+    await signup(user.email, user.password);
+  };
+  var [visible, setVisible] = useState("true");
+  var [texto, setTexto] = useState("Mostrar");
+
+  const change = () => {
+    setVisible(!visible);
+    var pass = document.getElementById("password");
+    if (visible) {
+      pass.type = "text";
+      setTexto("Ocultar contraseña");
+    } else {
+      pass.type = "password";
+        setTexto("Mostrar contraseña");
+    }
+  };
+
+  const login = () => {
+    router.push('/CustomLog') 
   }
 
   return (
@@ -32,7 +49,7 @@ const NewRegister = (props) => {
             <br></br>
             <h4 style={{ padding: "10px" }}>Registráte con tu Email</h4>
             <p style={{ fontSize: "small" }}>Ingresá tus datos</p>
-            {error && <p>{error}</p>}  
+            {error && <p>{error}</p>}
             <Form>
               <FloatingLabel label="Email address" className="mb-3">
                 <Form.Control
@@ -66,6 +83,17 @@ const NewRegister = (props) => {
             {/* <p style={{ fontSize: "small", marginTop: "10px" }}>
         Olvidé mi contraseña
       </p> */}
+            <p class="d-flex justify-content-end">
+              <button
+                className={styles.cs}
+                id="btnPass"
+                onClick={() => {
+                  change();
+                }}
+              >
+                {texto}
+              </button>
+            </p>
             <button
               type="submit"
               // onClick={(e) => {
@@ -78,7 +106,7 @@ const NewRegister = (props) => {
             >
               Registrarme
             </button>
-            <button href="/CustomLog" className={styles.botonLogin}>
+            <button onClick={()=>{login()} } className={styles.botonLogin}>
               Ya tengo cuenta
             </button>
           </div>
